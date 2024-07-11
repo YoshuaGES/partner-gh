42975da9471555050ea262d89268620176637614
YoshuaGES <auhsoywatchandlearn@gmail.com>
2024-07-11 16:55:09 +0700

    ddd

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import {
 
 import { imagePartner1 } from "./services/partner.js";
 import { imagePartner2 } from "./services/partner.js";
+import { imageVendor1 } from "./services/vendor.js";
+import { imageVendor2 } from "./services/vendor.js";
 
 const App = () => {
   return (
     <div className="bg-gray-100 p-20 min-h-screen">
-      <h1 className="font-poppins text-2xl font-bold text-center p-10">
+      <h1 className="font-poppins text-2xl font-bold text-center p-20">
         Our Client
       </h1>
       <div className="grid grid-cols-8 gap-4 p-2 animate-scroll-left">
@@ -20,7 +22,7 @@ const App = () => {
             key={image.id}
             className="bg-white flex justify-center items-center rounded-lg shadow-lg cursor-pointer"
           >
-            <img src={image.title} className="h-full" />
+            <img src={image.title} className="h-auto p-2" />
           </div>
         ))}
       </div>
@@ -30,7 +32,7 @@ const App = () => {
             key={image.id}
             className="bg-white flex justify-center items-center rounded-lg shadow-lg cursor-pointer"
           >
-            <img src={image.title} className="h-full" />
+            <img src={image.title} className="h-auto p-2" />
           </div>
         ))}
       </div>
@@ -40,7 +42,7 @@ const App = () => {
             key={image.id}
             className="bg-white flex justify-center items-center rounded-lg shadow-lg cursor-pointer"
           >
-            <img src={image.title} className="h-full" />
+            <img src={image.title} className="h-auto p-2" />
           </div>
         ))}
       </div>
@@ -50,30 +52,53 @@ const App = () => {
             key={image.id}
             className="bg-white flex justify-center items-center rounded-lg shadow-lg cursor-pointer"
           >
-            <img src={image.title} className="h-full" />
+            <img src={image.title} className="h-auto p-2" />
           </div>
         ))}
       </div>
       <h1 className="font-poppins text-2xl font-bold text-center p-20">
         Our Partner
       </h1>
-      <div className="grid grid-cols-8 gap-4 p-2 animate-scroll-left">
+      <div className="grid grid-cols-7 gap-4 p-2 animate-scroll-left">
         {imagePartner1.map((image) => (
           <div
             key={image.id}
             className="bg-white flex justify-center items-center rounded-lg shadow-lg cursor-pointer"
           >
-            <img src={image.title} className="h-1/2" />
+            <img src={image.title} className="h-auto p-2" />
           </div>
         ))}
       </div>
-      <div className="grid grid-cols-8 gap-4 p-2 animate-scroll-right">
+      <div className="grid grid-cols-7 gap-4 p-2 animate-scroll-right">
         {imagePartner2.map((image) => (
           <div
             key={image.id}
             className="bg-white flex justify-center items-center rounded-lg shadow-lg cursor-pointer"
           >
-            <img src={image.title} className="h-1/2" />
+            <img src={image.title} className="h-auto p-2" />
+          </div>
+        ))}
+      </div>
+      <h1 className="font-poppins text-2xl font-bold text-center p-20">
+        Our Vendor
+      </h1>
+      <div className="grid grid-cols-4 gap-4 p-2 animate-scroll-left">
+        {imageVendor1.map((image) => (
+          <div
+            key={image.id}
+            className="bg-white flex justify-center items-center rounded-lg shadow-lg cursor-pointer"
+          >
+            <img src={image.title} className="h-auto p-2" />
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-5 gap-4 p-2 animate-scroll-right">
+        {imageVendor2.map((image) => (
+          <div
+            key={image.id}
+            className="bg-white flex justify-center items-center rounded-lg shadow-lg cursor-pointer"
+          >
+            <img src={image.title} className="h-auto p-2" />
           </div>
         ))}
       </div>
---
+++ src/services/vendor.js
@@ -0,0 +1,42 @@
+export const imageVendor1 = [
+  {
+    id: 1,
+    title: "/public/Logo/Vendor Logo/vendor-1.jpg",
+  },
+  {
+    id: 2,
+    title: "/public/Logo/Vendor Logo/vendor-2.jpg",
+  },
+  {
+    id: 3,
+    title: "/public/Logo/Vendor Logo/vendor-3.jpg",
+  },
+  {
+    id: 4,
+    title: "/public/Logo/Vendor Logo/vendor-4.jpg",
+  },
+];
+
+export const imageVendor2 = [
+  {
+    id: 5,
+    title: "/public/Logo/Vendor Logo/vendor-5.jpg",
+  },
+  {
+    id: 6,
+    title: "/public/Logo/Vendor Logo/vendor-6.jpg",
+  },
+  {
+    id: 7,
+    title: "/public/Logo/Vendor Logo/vendor-7.jpg",
+  },
+  {
+    id: 8,
+    title: "/public/Logo/Vendor Logo/vendor-8.jpg",
+  },
+  {
+    id: 9,
+    title: "/public/Logo/Vendor Logo/vendor-9.jpg",
+  },
+];
+
